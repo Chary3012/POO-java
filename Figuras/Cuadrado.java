b4c20b036185e97421755ba1e3493f6fcf971e87
Chary3012 <a22216878@alumnos.uady.mx>
2023-10-02 17:13:06 -0600

    hola no se que hacer

--- Figuras/Cuadrado.java
+++ Figuras/Cuadrado.java
@@ -2,8 +2,13 @@ package Figuras;
 
 public class Cuadrado extends Figura {
 
+    //hola, este es un nuevo comentario
     public Cuadrado(double var1, double var2) {
         super(var1, var2);
+    }
+
+    public void nosequehacer(){
+        System.out.println("Hola");
     }
 
     @Override
