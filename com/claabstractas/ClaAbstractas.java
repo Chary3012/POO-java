4674c21bbfe8173e2a19a410eda5a490b751711c
Carlos Coronado <carlocoronadosilva975@gmail.com>
2023-10-08 23:06:45 -0600

    Estudio de interfaces

---
+++ com/claabstractas/ClaAbstractas.java
@@ -0,0 +1,7 @@
+package com.claabstractas;
+
+public class ClaAbstractas {
+    public static void main(String[] args){
+
+    }
+}
---
+++ com/claseHerencia/Herencia.java
@@ -0,0 +1,11 @@
+package com.claseHerencia;
+
+public class Herencia {
+
+    public static void main(String[] args) {
+        Empleado obj1 = new Empleado();
+
+        obj1.getNum_empleado();
+        obj1.get
+    }
+}
